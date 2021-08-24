006f41e6c0eed91c7b9c8df875d8399623ccf5aa
daos21912 <84862112+Daos21912@users.noreply.github.com>
2021-08-24 19:55:54 -0400

    updated style for form

--- campaign/app.py
+++ campaign/app.py
@@ -37,24 +37,44 @@ def create_app():
 
     @APP.route("/predict", methods=['POST'])
     def predict():
-        data = {'id': [9999999999],
+        data0 = {'id': [9999999999],
             'Primary Category':[request.values['Primary Category']],
             'Subcategory':[request.values['Subcategory']],
             'Backers Count':[request.values['Backers Count']],
             'Campaign Goal':[request.values['Campaign Goal']],
             'Amount Pledged':[request.values['Amount Pledged']],
-            'Staff Pick':[request.values['Staff Pick']],
+            'Staff Pick':[0],
             'Description Length':[request.values['Description Length']],
             'Campaign Length':[request.values['Campaign Length']],
             'Holiday Season':[request.values['Holiday Season']],
             'Campaign Launch Length':[request.values['Campaign Launch Length']]
         }
-        df = pd.DataFrame(data)
-        df.set_index('id', drop=True, inplace=True)
+        data1 = {'id': [9999999999],
+            'Primary Category':[request.values['Primary Category']],
+            'Subcategory':[request.values['Subcategory']],
+            'Backers Count':[request.values['Backers Count']],
+            'Campaign Goal':[request.values['Campaign Goal']],
+            'Amount Pledged':[request.values['Amount Pledged']],
+            'Staff Pick':[1],
+            'Description Length':[request.values['Description Length']],
+            'Campaign Length':[request.values['Campaign Length']],
+            'Holiday Season':[request.values['Holiday Season']],
+            'Campaign Launch Length':[request.values['Campaign Launch Length']]
+        }
+        df0 = pd.DataFrame(data0)
+        df0.set_index('id', drop=True, inplace=True)
+        df1 = pd.DataFrame(data1)
+        df1.set_index('id', drop=True, inplace=True)
         predictor = pickle.load(open('test_forest.pkl', 'rb'))
-        prediciton = predictor.predict(df)
-        if prediciton == 1:
+        prediciton0 = predictor.predict(df0)
+        prediciton1 = predictor.predict(df1)
+        if prediciton0 == 1 and prediciton1 == 1:
             pred = "this campaign is likely to succeed"
+        elif prediciton0 == 1 and prediciton1 == 0:
+            pred = "this campaign is more likely to succeed without getting picked by kickstarter staff"
+        elif prediciton0 == 0 and prediciton1 == 1:
+            pred = """this campaign is more likely to succeed if you get picked by kickstarter staff.<br/>
+                <a href='https://www.kickstarter.com/blog/introducing-projects-we-love-badges'>Click here to find out more</a>"""
         else:
             pred = "this campaign is likely to fail"
         return render_template('predict.html', pred=pred)
